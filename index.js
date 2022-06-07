b0b5b38a7f78e01a7534ff407f01f6a0121034c5
Allan Souza <allanzissimo@gmail.com>
2022-06-06 22:50:53 -0300

    Implementação da quarta função e acrescimo da possibilidade do switch na primeira

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 // PEDRA, PAPEL E TESOURA
+
 const escolhaDoUsuario = () => {
     let userInput = prompt("Escolha pedra, papel ou tesoura!!!")
 
     if(userInput.toLowerCase() == 'pedra') {
         userInput = 0
-        document.write(userInput)
     }else if(userInput.toLowerCase() == 'papel') {
         userInput = 1
-        document.write(userInput)
     }else if(userInput.toLowerCase() == 'tesoura') {
         userInput = 2
-        document.write(userInput)
     }else{
         document.write("Valor inválido, por favor digite apenas: pedra, papel ou tesoura!!!")
         userInput = 3
     }
 
+    /*switch(userInput.toLowerCase()) {
+        case 'pedra':
+            userInput = 0
+            break;
+        case 'papel':
+            userInput = 1
+            break;
+        case 'tesoura':
+            userInput = 2
+            break;
+        default:
+            document.write("Valor inválido, por favor digite apenas: pedra, papel ou tesoura!!!")
+            userInput = 3
+    }*/
+
     return userInput
 }
 
 const escolhaDoComputador = () => {
-    //let decisaoComp = ""
 
     let min = Math.ceil(0);
     let max = Math.floor(3);
     let result = Math.floor(Math.random() * (max - min)) + min;
 
-    /*if(result == 0) {
-        decisaoComp = "tesoura"
-    }else if(result == 1) {
-        decisaoComp = "papel"
-    }else if(result == 2) {
-        decisaoComp = "pedra"
-    }
-
-    return decisaoComp*/
     return result
 }
 
@@ -56,3 +59,11 @@ const determineVencedor = (escolhaDoUsuario, escolhaDoComputador) => {
     }
 }
 
+const iniciarJogo = () => {
+    let userChoice = escolhaDoUsuario();
+    let computerChoice = escolhaDoComputador();
+
+    determineVencedor(userChoice, computerChoice)
+}
+
+iniciarJogo()
